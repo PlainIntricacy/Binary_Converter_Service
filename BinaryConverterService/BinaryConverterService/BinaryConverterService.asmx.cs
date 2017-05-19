93b2dbca6799dd8770e68bd2f7d195dbb9b2ba25
PlainIntricacy <tudorlaptes@gmail.com>
2017-05-19 11:56:46 -0400

    added converter logic

--- BinaryConverterService/BinaryConverterService/BinaryConverterService.asmx.cs
+++ BinaryConverterService/BinaryConverterService/BinaryConverterService.asmx.cs
@@ -7,7 +7,7 @@ using System.Web.Services;
 namespace BinaryConverterService
 {
     /// <summary>
-    /// Summary description for BinaryConverterService
+    /// C# version of https://github.com/PlainIntricacy/Binary_Converter delivered as a web service
     /// </summary>
     [WebService(Namespace = "http://tempuri.org/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
@@ -18,14 +18,30 @@ namespace BinaryConverterService
     {
 
         [WebMethod]
+        //Receives an integer as input, and returns its binary equivalent as a string.
         public string DecimalToBinary(int input)
         {
-            return "";
+            string outputString = "";
+            while (input >= 1)
+            {
+                outputString += (input % 2);
+                input /= 2;
+            }
+            return outputString;
         }
         [WebMethod]
-        public string BinaryToDecimal(string input)
+        //Receives a binary string as input, and returns its integer equivalent.
+        public int BinaryToDecimal(string input)
         {
-            return "";
+            int output=0;
+            for(int i=0; i<input.Length; i++)
+            {
+                if (input[i] == '1')
+                {
+                    output += (int)Math.Pow(2, i);
+                }
+            }
+            return output;
         }
     }
 }
